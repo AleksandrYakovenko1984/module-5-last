cfe449fd88f2e5b69073129761811b53f3bb19ba
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2022-02-19 20:24:38 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import PokemonForm from "./components/PokemonInfo/FormPokemon";
+import PokemonInfo from "./components/PokemonInfo/PokemomInfo";
+// import IconButton from "./components/IconButton/IconButton";
+// import Modal from "./components/Modal/Modal";
+// import Clock from "./components/Clock/Clock";
+// import Reader from "./components/Reader/Reader";
+// import publication from "./publication.json";
+// import { ReactComponent as AddIcon } from "./icons/iconmonstr-plus-2.svg";
+// import { ReactComponent as IconClose } from "./icons/iconmonstr-trash-can-27.svg";
+// import Tabs from "./components/Tabs/Tab";
+// import tab from './tabs.json'
+
+class App extends Component {
+  state = {
+    pokemonName: "",
+  };
+  // state = {
+  //   showModal: false,
+  // };
+
+  // togleModal = () => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //   }));
+  // };
+
+  handleFormSubmit = (pokemonName) => {
+    this.setState({ pokemonName });
+  };
+  render() {
+    // const { showModal } = this.state;
+    return (
+      <>
+        <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
+        {this.state.loading && <h1>loading...</h1>}
+        {this.state.pokemon && <div>{this.state.pokemon.name}</div>}
+
+        {/* <Reader items={publication} /> */}
+        {/* <Clock /> */}
+
+        {/* <IconButton type="button" onClick={this.togleModal}>
+          <AddIcon width="40" height="40" fill="#fff" />
+        </IconButton>
+
+        {showModal && (
+          <Modal onClose={this.togleModal}>
+            <h1>привет это контент модалки как children</h1>
+            <p>
+              Идейные соображения высшего порядка, а также сложившаяся структура
+              организации требует определения и уточнения экономической
+              целесообразности принимаемых изменений. Разнообразный и богатый
+              опыт социально-экономическое развитие играет важную роль в
+              формировании системы обучения кадров, соответствующей насущным
+              потребностям. Для современного мира реализация намеченных плановых
+              заданий проверки влечёт за собой интересный процесс внедрения
+              модернизации новых принципов формирования материально-технической
+              и кадровой базы.
+            </p>
+            <IconButton type="button" onClick={this.togleModal}>
+              <IconClose width="25" height="25" fill="#fff" />
+            </IconButton>
+          </Modal>
+        )} */}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PokemonInfo/PokemomInfo.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PokemonDataView from "./PokemonData";
+export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+    error: null,
+    status: "idle",
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pokemonName !== this.props.pokemonName) {
+      this.setState({ status: "pending" });
+      setTimeout(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonName}`)
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            return Promise.reject(
+              new Error(`нет Покемона с именем ${this.props.pokemonName}`)
+            );
+          })
+          .then((pokemon) => this.setState({ pokemon, status: "resolved" }))
+          .catch((error) => this.setState({ error, status: "rejected" }));
+      }, 1000);
+    }
+  }
+
+  render() {
+    const { pokemon, error, status } = this.state;
+
+    if (status === "idle") {
+      return <div>Введите имя покемона.</div>;
+    }
+    if (status === "pending") {
+      return <div>загружаем...</div>;
+    }
+    if (status === "rejected") {
+      return <h1>{error.message}</h1>;
+    }
+    if (status === "resolved") {
+      return <PokemonDataView pokemon={pokemon} />;
+    }
+  }
+}
